8d0856d1f08b1d8865e5f896f59357a30d67439f
Sobhit Kumar <sobhit2010@gmail.com>
2018-02-07 21:07:14 +0530

    Add googlemap streetview to load image in background

--- js/script.js
+++ js/script.js
@@ -13,9 +13,30 @@ function loadData() {
 
     // load streetview
 
+    var streetName = $("#street").val();
+    var cityName = $("#city").val();
+    var address = streetName + ", "+ cityName;
+
+    $greeting.text("So you want to live at "+ address +"?");
+
+    var streetViewURL = "https://maps.googleapis.com/maps/api/streetview?size=600x300&location="+address+"";
+    $body.append('<img class="bgimg" src="' + streetViewURL + '">');
+
     // YOUR CODE GOES HERE!
 
+    // var streetStr = $("#street").val();
+    // var cityStr = $("#city").val();
+    // var address = streetStr + ", " + cityStr;
+    
+    // $("label").css("color","#fafafa");
+    // $greeting.text("So, you want to live at " + address + "?").css("color","#fafafa");
+    // var streetViewURL = "http://maps.googleapis.com/maps/api/streetview?size=600x300&location="+ address + " ";
+    // $body.append('<img class="bgimg" src="'+streetViewURL+'">');
+   
+
     return false;
+
+    
 };
 
 $('#form-container').submit(loadData);
